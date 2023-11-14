0eb986b7c985ddd06e798fd4c3caea2a2e2b8421
INJAM UL HAQUE <injamtanvir@gmail.com>
2023-11-14 18:38:28 +0600

    A

--- Module 2.5_Practice Day 01/03.cpp
+++ Module 2.5_Practice Day 01/03.cpp
@@ -2,3 +2,29 @@
 
 #include<bits/stdc++.h>
 using namespace std;
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+}
+
+int main() {
+    int N;
+    cin >> N;
+    int A[N];
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+    bubbleSort(A, N);
+
+    for (int i = 0; i < N; i++) {
+        cout << A[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
--- Module 2.5_Practice Day 01/tempCodeRunnerFile.cpp
+++ Module 2.5_Practice Day 01/tempCodeRunnerFile.cpp
@@ -1,18 +1,31 @@
 //InjamTanvir
- 
+
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
-    int a, b;
-    cin >> a >> b;
-    int plus = a + b;
-    long long int multiplication = a * b;
-    int Minus = a -b;
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        swap(arr[minIndex], arr[i]);
+    }
+}
+int main() {
+    int N;
+    cin >> N;
+    int A[N];
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+    selectionSort(A, N);
 
-    cout << a << " + " << b << " = " << plus << endl;
-    cout << a << " * " << b << " = " << multiplication << endl;
-    cout << a << " - " << b << " = " << Minus << endl;
+    for (int i = 0; i < N; i++) {
+        cout << A[i] << " ";
+    }
+    cout << endl;
 
     return 0;
- 
 }
